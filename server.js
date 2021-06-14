2177499a862e2957eb1301dd21cd273389245b1a
jackdelv <jackdelv39@gmail.com>
2021-06-14 11:44:18 -0400

    Added post route for api/fileanalyze using multer

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
 var app = express();
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +12,9 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+    res.json({'name': req.file.originalname, 'type': req.file.mimetype, 'size': req.file.size});  
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
