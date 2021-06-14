08ac7c08e6c5db3e8e139d29b3b1a1aa41d1c81e
jackdelv <85182563+jackdelv@users.noreply.github.com>
2021-06-14 11:25:50 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", function(req, res, next) {res.sendFile(__dirname + "/views/index.html)});
+app.get("/", function(req, res, next) {res.sendFile(__dirname + "/views/index.html")});
