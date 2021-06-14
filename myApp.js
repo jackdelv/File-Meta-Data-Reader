6513de9a3f8941d90c14422754d726801691aef1
jackdelv <85182563+jackdelv@users.noreply.github.com>
2021-06-14 16:52:28 -0400

    Delete myApp.js

--- myApp.js
+++
@@ -1,4 +0,0 @@
-var express = require('express');
-var app = express();
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", function(req, res, next) {res.sendFile(__dirname + "/views/index.html")});
